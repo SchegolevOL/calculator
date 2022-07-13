7219efe611784999acbaf77c30f8943bb77a8852
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-07-13 10:48:27 +0500

    added buttons 1 - 9 arithmetic action buttons, added string composition methods

---
+++ Calculator.App/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string? _input = null;
+        private bool FlagPoint = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonClickNumber(object sender, RoutedEventArgs e)
+        {
+            if ((string?)((sender as Button)?.Content) == "0" && _input == null) return;
+            _input += (sender as Button)?.Content;
+            Input.Text = _input;
+        }
+        private void ButtonClickPoint(object sender, RoutedEventArgs e)
+        {
+            if (_input?[_input.Length - 1] == ' ' || FlagPoint == true || _input == null) return;
+            FlagPoint = true;
+            _input += (sender as Button)?.Content;
+            Input.Text = _input;
+        }
+        private void ButtonAction(object sender, RoutedEventArgs e)
+        {
+            if (_input?[_input.Length - 1] == ' '|| _input==null) return;
+            _input += (" " + (sender as Button)?.Content) + " ";
+            FlagPoint = false;
+            Input.Text = _input;
+        }
+
+    }
+}
