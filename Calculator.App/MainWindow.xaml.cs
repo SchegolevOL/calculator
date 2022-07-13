d9023a1e63e62d114a428f205d8bb7db41d4c68f
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-07-13 16:46:09 +0500

    add Metods ButtonClickCE ButtonClickC ButtonClickDel

--- Calculator.App/MainWindow.xaml.cs
+++ Calculator.App/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace Calculator.App
     public partial class MainWindow : Window
     {
         private string? _input = null;
-        private bool FlagPoint = false;
+        private bool _flagPoint = false;
+        private string _result;
         public MainWindow()
         {
             InitializeComponent();
@@ -36,18 +37,89 @@ namespace Calculator.App
         }
         private void ButtonClickPoint(object sender, RoutedEventArgs e)
         {
-            if (_input?[_input.Length - 1] == ' ' || FlagPoint == true || _input == null) return;
-            FlagPoint = true;
+            if (_input?[_input.Length - 1] == ' ' || _flagPoint == true || _input == null) return;
+            _flagPoint = true;
             _input += (sender as Button)?.Content;
             Input.Text = _input;
         }
-        private void ButtonAction(object sender, RoutedEventArgs e)
+        private void ButtonClickAction(object sender, RoutedEventArgs e)
         {
-            if (_input?[_input.Length - 1] == ' '|| _input==null) return;
+            if (_input?[_input.Length - 1] == ' ' || _input == null) return;
             _input += (" " + (sender as Button)?.Content) + " ";
-            FlagPoint = false;
+            _flagPoint = false;
             Input.Text = _input;
         }
+        private void ButtonClickEnter(object sender, RoutedEventArgs e)
+        {
+            double value = 0, result = 0;
+            if (_input?[_input.Length - 1] == ' ' || _input == null) return;
+            string[] partsExpression = _input.Split(' ');
+            for (int i = 1; i < partsExpression.Length; i += 2)
+            {
+                switch (partsExpression[i])
+                {
+                    case "+":
+                        value = double.Parse(partsExpression[i - 1]) + double.Parse(partsExpression[i + 1])+0.00000001;
+                        break;
+                    case "-":
+                        value = double.Parse(partsExpression[i - 1]) - double.Parse(partsExpression[i + 1]);
+                        break;
+                    case "*":
+                        value = double.Parse(partsExpression[i - 1]) * double.Parse(partsExpression[i + 1]);
+                        break;
+                    case "/":
+                        value = double.Parse(partsExpression[i - 1]) / double.Parse(partsExpression[i + 1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            _result = value.ToString();
+            OutResult.Text = _result;
+            _input = null;
+            Input.Text = "0";
+            _flagPoint = false;
+        }
 
+        private void ButtonClickDel(object sender, RoutedEventArgs e)
+        {
+            int count = 1;
+            if (_input == null) return;
+            if (_input[_input.Length - 1] == ',')
+            {
+                _flagPoint = false;
+                return;
+            }
+            if (_input[_input.Length - 1] == ' ') count = 3;
+            _input = _input.Remove(_input.Length - count, count);
+            
+            if (_input.Length == 0) _input = null;
+
+            if (_input == null) Input.Text = "0";
+            else Input.Text = _input;
+        }
+        private void ButtonClickCE(object sender, RoutedEventArgs e)
+        {
+            if (_input == null) return;
+            while (_input.Length != 0)
+            {
+                if (_input[_input.Length - 1] == ' ') break;
+                _input = _input.Remove(_input.Length - 1,1);
+            }
+            if (_input.Length == 0) _input = null;
+            if (_input == null) Input.Text = "0";
+            else Input.Text = _input;
+        }
+        private void ButtonClickC(object sender, RoutedEventArgs e)
+        {
+            if (_input == null) return;
+            while (_input.Length != 0)
+            {               
+                _input = _input.Remove(_input.Length - 1, 1);
+            }
+            if (_input.Length == 0) _input = null;
+            if (_input == null) Input.Text = "0";
+            else Input.Text = _input;
+        }
     }
 }
